8fc996be2ce131b874d13ee7835eae845f068556
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-04-29 10:34:05 +0900

    added all features on to the views

--- profiles_PROJECT/profiles_api/models.py
+++ profiles_PROJECT/profiles_api/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
+
 
 class UserProfileManager(BaseUserManager):
-    """Manager for user profile"""
+    """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
@@ -14,23 +16,25 @@ class UserProfileManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
-        user.set_password(password) #django model to encrypte password
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self,email,name,password):
+    def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
-        user= self.create_user(email, name, password)
+        user = self.create_user(email, name, password)
 
-        user.is_superuser = True #automatically created by PermissionMixin
-        user.is_staff = True 
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
+
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
-    email = models.EmailField(max_length=255, unique=True) #ensure all emails are unique
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -45,10 +49,23 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.name
 
     def get_short_name(self):
-        """Retrieve short name of user"""
+        """Retrieve shot name of user"""
         return self.name
-
 
     def __str__(self):
         """Return string representation of our user"""
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
